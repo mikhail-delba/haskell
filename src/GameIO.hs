f7cf8299b67b0a9f221772adf6c12a1ff72d7e77
mikhail-delba <mikhail.kedrov@gmail.com>
2022-04-07 04:43:28 +0300

    3 milestone

--- src/GameIO.hs
+++ src/GameIO.hs
@@ -5,103 +5,193 @@ import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.IO.Game
 
-
 -- a player controlls a paddle, bouncing a ball off to the wall above.
 -- There are also borders right and left to the player
 -- if he succesfully hits the ball off, he gets +1 score. 
--- if ball goes past the paddle, player looses.
+-- if the ball goes past the paddle, player looses.
+-- the ball increases it spead when it is hit
+-- gameOver mechanic not implemented yet, headed for scoreboard
 
---CONSTANTS
+-- CONSTANTS --
 
 -- window size
-windowWidth = 300
-windowHeight = 300
+windowWidth, windowHeight :: Int
+windowWidth = 500
+windowHeight = 500
+
+paddleMaxX = (fromIntegral windowWidth/2) - paddleLength - wallWidth
 
 -- wall
-wallHeight = 10
-wallWidth = windowWidth
+wallWidth = 10
 
 -- paddle
-paddleWidth = 2
-paddleLength = 10
+paddleWidth = 10
+paddleLength = 50
 paddleSpeed = 10
 
---ball
+-- ball
 ballRad = 10
-ballSpeed = 5 -- MIGHT SPEED UP OVER THE GAME => INTO GameState???
 
+-- сolors
 fieldColor :: Color
-fieldColor = green
+fieldColor = dark green
 
 ballColor :: Color
 ballColor = white
 
 paddleColor :: Color
-paddleColor = brown
+paddleColor = light (light azure)
 
--- DATA/TYPES
+wallColor :: Color
+wallColor = greyN 0.2
 
-type Pos = (Int, Int) -- player/ball position...
-type Dir = (Int, Int) -- ...and direction
+borderColor :: Color
+borderColor = light green
 
-data Ball = Ball { -- may be a distinct data type for a ball instead of GS fields?
-  ballPos :: Pos,
-  ballDir :: Dir
-  }
+-- DATA/TYPES --
+type Pos = (Float, Float)
+type Dir = (Float, Float)
 
-data Move = MoveRight | MoveLeft | NoMovement
+data Move = MoveRight | MoveLeft | NoMovement deriving Show
 
-data ScoreBoard = Scores {} -- a table of player's scores, storing top 5 up to date
+data ScoreBoard = Scores {} -- a table of player's scores, storing top 5 up to date (IND. PART)
 
-data GameState = GS { 
-  ballPos :: Pos,
-  ballDir :: Dir,
-  paddlePos :: Pos,
-  paddleMove :: Move,
-  score :: Int
-} deriving Show -- want to be able to show player's score with a number
+-- structure to store current game's data
+data GameState = GS 
+  { ballPos :: Pos,
+    ballDir :: Dir,
+    ballSpeed :: Float,
+    paddlePos :: Pos,
+    paddleMove :: Move,
+    gameOver :: Bool, -- if lost, then TRUE
+    score :: Int
+  } deriving Show --in order to show player's score as a number
+
+-- Initial values --
+initBallPos = (0.0, 0.0)
+initDir = (1.0, 1.0)
+initSpeed = 200.0
+initPaddlePos = (0.0, -150.0)
 
 initialState :: GameState
-initialState = GS (0, 0) (1, 1) (5,5) NoMovement 0
-
--- FUNCTIONS
+initialState = GS initBallPos initDir initSpeed initPaddlePos NoMovement False 0
 
 run :: IO ()
 run = do 
     gen <- getStdGen
     startGame gen
-  
+
+-- game state updater -- 
 updateApp :: Float -> GameState -> GameState
--- running all the functions below in a row
+updateApp t = ballMissed . physicsCollision . paddleControl . physicsBall t -- check gameOver here???
 
 startGame :: StdGen -> IO ()
--- startGame gen = play ... <gloss rendering specs using updateApp>
+startGame gen = play (InWindow "Ping-Pong" (windowWidth, windowHeight) (500, 200)) 
+                          fieldColor 60 initialState render handleEvent updateApp
 
--- moving Paddle in one frame
-movePaddle :: GameState -> Move -> GameState 
-movePaddle gs MoveLeft = gs {paddlePos = (x - paddleSpeed, y)}
-movePaddle gs MoveRight = gs {paddlePos = (x + paddleSpeed, y)}
-movePaddle gs NoMove = gs
+-- PHYSICS --
 
--- moving ball for some distance in ballDir direction
-moveBall :: GameState -> Float -> GameState
-moveBall gs dist = gs {ballPos = (x + dx, y + dy)} 
+-- calculating ball's motion based on current position and direction
+physicsBall :: Float -> GameState -> GameState
+physicsBall t gs@GS {ballPos = (x, y), ballDir = (dx, dy)} = gs { ballPos = (x', y') }
+  where
+    x' = x + dx * t * (ballSpeed gs)
+    y' = y + dy * t * (ballSpeed gs)
 
--- checking if ball collides with anything
+paddleControl :: GameState -> GameState
+paddleControl gs@GS {paddlePos = (x, y), paddleMove = move} = gs { 
+                                   paddlePos = checkMaxX $ paddleMovePos (x,y) move }
+  where 
+    checkMaxX :: Pos -> Pos
+    checkMaxX (x, y) = (max (-paddleMaxX) (min paddleMaxX x), y)
+
+    -- depending on the input, moving the paddle --
+    paddleMovePos :: Pos -> Move -> Pos
+    paddleMovePos (x, y) MoveLeft = (x - paddleSpeed, y)
+    paddleMovePos (x, y) MoveRight = (x + paddleSpeed, y)
+    paddleMovePos pos NoMovement = pos
+
+-- check if the ball collided with a paddle...
+checkPaddleCollision :: Pos -> Pos -> Bool
+checkPaddleCollision (bx, by) (px, py) = checkX && checkY -- check if the ball falls for both axes
+  where
+    checkX = (bx - ballRad <= px + paddleLength) && (bx - ballRad >= px - paddleLength)
+    checkY = (by - ballRad>= py - paddleWidth) && (by - ballRad <= py + paddleWidth)
+
+-- check if the ball collided with the upper wall
 checkWallCollision :: Pos -> Bool
-checkPaddleCollision :: Pos -> Bool
+checkWallCollision (x, y) =  y + ballRad >= fromIntegral windowWidth / 2
 
--- functions using checks above to update the GS
-wallHit :: GameState -> GameState
-borderHit :: GameState -> GameState
-paddleHit :: GameState -> GameState -- if TRUE, player's score +1!, other GS data kept the same
-ballMissed :: GameState -> GameState -- if TRUE, player's score -1!, return to initial position
+-- check if the ball collided with either border
+checkBorderCollision :: Pos -> Bool
+checkBorderCollision (x, y) = leftBorderCollision || rightBorderCollision
+  where
+    leftBorderCollision = x - ballRad <= -fromIntegral windowWidth / 2
+    rightBorderCollision = x + ballRad >= fromIntegral windowWidth / 2
 
+-- the overall function that checks any possible collision & hit
+physicsCollision :: GameState -> GameState
+physicsCollision = borderHit . wallHit . paddleHit
+    where 
+    paddleHit :: GameState -> GameState
+    paddleHit gs@GS {ballDir = (x, y)} = gs { ballDir = (x, y'), ballSpeed = speed, score = scoreValue}
+      where
+        collided = checkPaddleCollision (ballPos gs) (paddlePos gs)
+        y' = if collided then -y else y
+        speed = if collided
+             then ballSpeed gs + 10 -- ball speed + 10 when hit by a paddle
+             else ballSpeed gs
+        scoreValue = if collided
+              then score gs + 1 else score gs
+    
+    wallHit :: GameState -> GameState
+    wallHit gs@GS {ballPos = (px, py), ballDir = (x, y)} = gs { ballDir = (x, y') }
+      where y' = if checkWallCollision (px, py) then -y else y
+
+    borderHit :: GameState -> GameState
+    borderHit gs@GS {ballPos = (px, py), ballDir = (x, y)} = gs { ballDir = (x', y) }
+      where x' = if checkBorderCollision (px, py) then -x else x
+
+-- if the ball fell behind the paddle...
+ballMissed :: GameState -> GameState
+ballMissed gs@GS {ballPos = (x, y)} = if y <= - fromIntegral windowHeight/2 
+                                        then gs { ballPos = initBallPos, ballSpeed = initSpeed,
+                                         paddlePos = initPaddlePos, 
+                                         gameOver = True, 
+                                         score = 0 } -- lost => gameOver = TRUE
+                                      else gs
+
+-- EVENT HANDLING --
 handleEvent :: Event -> GameState -> GameState
--- Handling the event: A - moving left, D - moving right
 handleEvent (EventKey (Char 'a') Down _ _) gs = gs { paddleMove = MoveLeft }
 handleEvent (EventKey (Char 'a') Up _ _) gs = gs { paddleMove = NoMovement }
 handleEvent (EventKey (Char 'd') Down _ _) gs = gs { paddleMove = MoveRight }
 handleEvent (EventKey (Char 'd') Up _ _) gs = gs { paddleMove = NoMovement }
+handleEvent _ gs = gs
 
--- RENDERING...
+
+-- RENDERING --
+drawPaddle :: Pos -> Picture
+drawPaddle (x, y) = pictures [ translate x y $ 
+      color paddleColor $
+       rectangleSolid (2 * paddleLength) (2 * paddleWidth)]
+
+drawBorder :: Float -> Picture
+drawBorder dist = translate dist 0 $ color borderColor $ 
+                  rectangleSolid wallWidth (fromIntegral windowHeight)
+
+render :: GameState -> Picture
+render gs@GS {ballPos = (x, y), paddlePos = (px, py)} = pictures [ballPic, drawPaddle (px, py), 
+                                                              wallPic, borderPics, scoreText] -- rendering a list of pictures translated below
+  where
+    ballPic = translate x y $ color ballColor $ circleSolid ballRad
+    
+    --upper wall
+    wallPic = translate 0 (fromIntegral windowWidth/2) $ color wallColor $ 
+                      rectangleSolid (fromIntegral windowWidth) wallWidth
+    --side borders
+    borderPics = pictures [drawBorder (-fromIntegral windowWidth/2), 
+                        drawBorder (fromIntegral windowWidth/2)]
+
+    scoreText = translate (-50) 150 $
+      scale 0.2 0.2 $ color white $ text ("SCORE: " ++ show (score gs))
